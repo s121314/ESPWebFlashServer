208fa2905c9af5898c05c6ee65243c245b02267d
Scott <treii28@gmail.com>
2017-08-27 13:10:39 -0400

    Add files via upload
    base files

---
+++ ESPWebFlashServer.ino
@@ -0,0 +1,150 @@
+/*
+ * WebFlashServer.ino
+ *
+ * Created: 8/27/2017 12:44:09 AM
+ * Author: swood
+ */
+
+#ifndef Arduino_h
+#include <Arduino.h>
+#endif
+
+#ifndef ESP8266WEBSERVER_H
+#include <ESP8266WebServer.h>
+#endif
+
+#ifndef WiFi_h
+#include <ESP8266WiFi.h>
+#endif
+#ifndef WiFi_h
+#include <ESP8266WiFi.h>
+#endif
+#ifndef wificlient_h
+#include <WiFiClient.h>
+#endif
+#ifndef IPAddress_h
+#include <IPAddress.h>
+#endif
+
+#ifndef WEBFILES_H_
+#include "webfiles.h"
+#endif
+
+ESP8266WebServer APWebServer;
+IPAddress myIP;
+
+int findFlashIndexByPath(String path) {
+	int NumFiles = sizeof(files) / sizeof(struct t_websitefiles);
+	for (int i = 0; i < NumFiles; i++) {
+		if (path == files[i].path)
+		return i;
+	}
+	return -1;
+}
+
+bool loadIndexFromFlash(int i) {
+	if (i >= 0) {
+		_FLASH_ARRAY<uint8_t>* filecontent;
+		String dataType = "text/plain";
+		unsigned int len = 0;
+
+		dataType = String(files[i].mime);
+		len = files[i].len;
+		if (files[i].enc != "")
+			APWebServer.sendHeader("Content-Encoding", files[i].enc);
+		APWebServer.setContentLength(len);
+		APWebServer.send(200, files[i].mime, "");
+
+		filecontent = (_FLASH_ARRAY<uint8_t>*)files[i].content;
+		filecontent->open();
+		WiFiClient APclient = APWebServer.client();
+		APclient.write(*filecontent, 100);
+		return true;
+	}
+	return false;
+}
+
+/* Flash.h load functions for web contet */
+bool loadPathFromFlash(String path) {
+	if (path.endsWith("/")) path += "index.htm";
+
+	int NumFiles = sizeof(files) / sizeof(struct t_websitefiles);
+	int index = findFlashIndexByPath(path);
+	if (index >= 0) {
+		Serial.print("serving content for path: ");
+		Serial.println(path);
+		return loadIndexFromFlash(index);
+	} else {
+		return false;
+	}
+}
+
+void handleNotFound() {
+	// try to find the file in the flash
+	if (loadPathFromFlash(APWebServer.uri())) return;
+
+	String message = "File Not Found\n\n";
+	message += "URI..........: ";
+	message += APWebServer.uri();
+	message += "\nMethod.....: ";
+	message += (APWebServer.method() == HTTP_GET) ? "GET" : "POST";
+	message += "\nArguments..: ";
+	message += APWebServer.args();
+	message += "\n";
+	for (uint8_t i = 0; i < APWebServer.args(); i++) {
+		message += " " + APWebServer.argName(i) + ": " + APWebServer.arg(i) + "\n";
+	}
+	message += "\n";
+	message += "FreeHeap.....: " + String(ESP.getFreeHeap()) + "\n";
+	message += "ChipID.......: " + String(ESP.getChipId()) + "\n";
+	message += "FlashChipId..: " + String(ESP.getFlashChipId()) + "\n";
+	message += "FlashChipSize: " + String(ESP.getFlashChipSize()) + " bytes\n";
+	message += "getCycleCount: " + String(ESP.getCycleCount()) + " Cycles\n";
+	message += "Milliseconds.: " + String(millis()) + " Milliseconds\n";
+	APWebServer.send(404, "text/plain", message);
+}
+
+void handleAPClient()
+{
+	APWebServer.handleClient();
+}
+
+/* access point setup */
+void setupAP() {
+	String apSSID = "WebFlashServer";
+	Serial.println(apSSID);
+
+	String apPass = "knock knock knock";
+	/* You can remove the password parameter if you want the AP to be open. */
+	WiFi.mode(WIFI_AP_STA); // set both AP and STA modes
+	WiFi.softAP(apSSID.c_str(), apPass.c_str());
+	myIP = WiFi.softAPIP();
+
+	Serial.println("AP IP address: ");
+	Serial.println(myIP);
+}
+
+void setupServer() {
+	APWebServer = ESP8266WebServer(80);
+	APWebServer.onNotFound(handleNotFound);
+	APWebServer.begin();
+	Serial.println("HTTP server started");
+}
+
+/* default arduino methods */
+
+void setup() {
+	Serial.begin(115200);
+	Serial.println();
+	delay(500);
+
+	setupAP();
+	setupServer();
+}
+
+void loop() {
+	for (int t = 0; t < 500; t++) {
+		APWebServer.handleClient();
+		delay(50);
+	}
+}
---
+++ hexify.pl
@@ -0,0 +1,221 @@
+#!/usr/bin/perl
+
+use Switch;
+
+use CSS::Packer;
+use JavaScript::Packer;
+use HTML::Packer;
+
+use Gzip::Faster ':all';
+use MIME::Types;
+use File::MimeInfo;
+use File::Basename;
+use File::Slurp;
+
+$gz = Gzip::Faster->new();
+$gz->level(9);
+
+my $outfile = "webfiles.h";
+my $webdir = "www";
+my $find_exe = "/usr/bin/find";
+my $findcmd = $find_exe . " " . $webdir . " -type f ";
+
+my $mt = MIME::Types->new();
+my $files = open(FINDSTRM, "$findcmd|") || die $!;
+my @filelist;
+while(my $line = <FINDSTRM>) {
+    chomp($line);
+    my %fileinfo;
+    $fileinfo{path} = $line;
+    print "processing file: $fileinfo{path}:\n";
+    (my $hName = $line) =~ s|/|__|g;
+    $hName =~ s/[\.\s]/_/g;
+    print "  using header name: $hName\n";
+    $fileinfo{name} = $hName;
+    $fileinfo{type} = mimetype($fileinfo{path});
+
+
+    my $bindat;
+    # read file normal
+    my $rawdat = read_file($fileinfo{path});
+    switch($fileinfo{type}) {
+        case /^text\/(html|xml|sgml)$/ {
+            $bindat = processHTML($rawdat, basename($fileinfo{path}));
+        }
+        # handle svg files as xml data - use html minifier and gzip
+        case /^(image|application)\/svg\+xml$/ {
+            $bindat = processHTML($rawdat, basename($fileinfo{path}));
+        }
+        case "text/css" {
+            $bindat = processCSS($rawdat, basename($fileinfo{path}));
+        }
+        case /^application\/(javascript|ecmascript)&/ {
+            $bindat = processJS($rawdat, basename($fileinfo{path}));
+        }
+        case /^text\/(plain|richtext|csv)$/ {
+            $bindat = processTxt($rawdat, basename($fileinfo{path}));
+        }
+    }
+
+    if(length($bindat) == 0) {
+        print "  raw-reading file $fileinfo{path}\n";
+        # assume binary file and read file direct in binmode
+        $bindat = read_file($fileinfo{path}, { binmode => ':raw' });
+        if($fileinfo{type} eq "application/gzip") {
+            $fileinfo{enc} = "gzip";
+        } elsif($fileinfo{type} eq "application/x-compress")
+        {
+            $fileinfo{enc} = "compress"
+        } else {
+            $fileinfo{enc} = ""
+        }
+    } else {
+        use bytes;
+        my $rawlen = length($rawdat);
+        my $binlen = length($bindat);
+        # use raw data if compressed isn't at least 10% smaller
+        if(($rawlen > 0) && ($binlen > ($rawlen * 0.9))) {
+            print "  -- compressed version not smaller, using raw data!\n";
+            $bindat = $rawdat;
+            $fileinfo{enc} = ""
+        } else {
+            $fileinfo{enc} = "gzip";
+        }
+    }
+
+    {
+        use bytes;
+        $fileinfo{len} = length($bindat);
+    }
+
+    $fileinfo{hexdat} = hexArray($bindat, 16);
+    push(@filelist,\%fileinfo);
+}
+close FINDSTRM;
+
+open(OUTFILE, ">", $outfile) || die $!;
+print OUTFILE '
+#ifndef __WEBFLASH_H__
+#include "WebFlash.h"
+#endif
+';
+
+foreach $f (@filelist) {
+    printf OUTFILE '
+FLASH_ARRAY(uint8_t, %s,
+%s
+);
+
+', $f->{name}, $f->{hexdat};
+}
+
+print OUTFILE '
+struct t_websitefiles {;
+  const char* path;
+  const char* mime;
+  const unsigned int len;
+  const char* enc;
+  const _FLASH_ARRAY<uint8_t>* content;
+} files[] = {
+';
+
+foreach $f (@filelist) {
+    printf OUTFILE '
+  {
+    .path    = "%s",
+    .mime    = "%s",
+    .len     = %d,
+    .enc     = "%s",
+    .content = &%s,
+  },', $f->{path}, $f->{type}, $f->{len}, $f->{enc}, $f->{name};
+}
+
+print OUTFILE '
+};
+';
+close OUTFILE;
+
+sub processTxt {
+    my $txt   = shift @_;
+    my $fName = shift @_;
+    print "  compressing text in $fName\n";
+    $gz->file_name($fName);
+    return $gz->zip($txt);
+
+}
+sub processHTML {
+    my $html  = shift @_;
+    my $fName = shift @_;
+    my $htmMin;
+    if($fName =~ /\.min\./) {
+        print "  minified html detected!\n";
+        $htmMin = $html;
+    } else {
+        print "  html minifying $fName\n";
+        my $packer = HTML::Packer->init();
+        $htmMin = $packer->minify( \$html, { remove_newlines => 1, remove_comments => 1, do_javascript => 'best', do_stylesheet => 'minify' } );
+        # don't use minified version if it's not at least 10% smaller
+        if(length($htmMin) > (length($html)*0.9)) {
+            print "  - minifying didn't help.\n";
+            $htmMin = $html;
+        }
+    }
+
+    $gz->file_name($fName);
+    return $gz->zip($htmMin);
+}
+sub processCSS {
+    my $css   = shift @_;
+    my $fName = shift @_;
+    my $packer = CSS::Packer->init();
+    my $cssMin;
+    if($fName =~ /\.min\./) {
+        print "  minified css detected!\n";
+        $cssMin = $css;
+    } else {
+        print "  css minifying $fName\n";
+        $cssMin = $packer->minify(\$css, { compress => 'minify', remove_comments => 1 } );
+        # don't use minified version if it's not at least 10% smaller
+        if(length($cssMin) > (length($css)*0.9)){
+            print "  - minifying didn't help.\n";
+            $cssMin = $css;
+        }
+    }
+    $gz->file_name($fName);
+    return $gz->zip($cssMin);
+}
+sub processJS {
+    my $js    = shift @_;
+    my $fName = shift @_;
+    my $jsMin;
+    if($fName =~ /\.min\./) {
+        print "  minified js detected!\n";
+        $jsMin = $js;
+    } else {
+        print "  javascript minifying $fName\n";
+        my $packer = JavaScript::Packer->init();
+        my $jsMin = $packer->minify(\$js, { compress => 'best', remove_comments => 1, remove_copyright => 1 } );
+        # don't use minified version if it's not at least 10% smaller
+        if(length($jsMin) > (length($js)*0.9)) {
+            print "  - minifying didn't help.\n";
+            $jsMin = $js;
+        }
+    }
+    $gz->file_name($fName);
+    return $gz->zip($jsMin);
+}
+
+sub hexArray {
+    my ($d, $maxCol) = @_;
+    if(!($maxCol > 0)) { $maxCol = 12 }; # default to 12 columns
+    my @fCol = ();
+    my @fRow = ();
+    for my $hc (unpack('(H2)*', $d)) {
+        push(@fCol, '0x'.$hc);
+        if(scalar @fCol >= $maxCol) {
+            push(@fRow, join(", ", @fCol));
+            @fCol = ();
+        }
+    }
+    return "  " . join(",\n  ", @fRow);
+}
